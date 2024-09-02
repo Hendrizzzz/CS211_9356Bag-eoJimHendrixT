55b7fa108c9ae3198bcd381b938a1b11d1c3e0ee
Jim Hendrix Bag-eo <jimhendrixbageo@gmail.com>
2024-09-02 23:10:12 +0800

    Finish the SinglyLinkedList
    Update the 3 linkedlists for some issues in the code and optimized the singlylinkedlist package

--- src/prelim/test.java
+++
@@ -1,8 +0,0 @@
-package prelim;
-
-public class test {
-    public static void main(String[] args) {
-        String string = "What the hell are you doing inside my house.You get the fucl outta my house for Gpd's sake, Pleaseeee, will you get out of my house man. I am living in peace here please don't disturb me anymnore, besides it is in the 12:00 am in the middle of the night at friday. Continue doing that tommorow but please shut the hell right now or I will call the police on you. You got that? huh? You know who I am?";
-        System.out.printf("%-10s%n", string);
-    }
-}
